2224b128f54eeee3e7d40f5360faf6a0db72818f
Jean-Pierre-A <jean-agudelo@outlook.com>
2024-04-04 20:42:46 -0500

    Añadimos JavaScript

---
+++ javascripts/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Full Stack Developer", "Data engineer", "Power Platform", "Runner"],
+    typeSpeed:100,
+    backSpeed:60,
+    backDelay:1000,
+    loop: true
+})
